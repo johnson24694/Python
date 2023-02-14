325b8ddb3973a5ee8ddae1375d97da571fe52cdd
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-02-14 10:24:09 -0600

    Added count positives and is array a Palindrome?

---
+++ W1D3.js
@@ -0,0 +1,35 @@
+// count positives
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > -1) {
+    countPositives += 1;
+    }
+}
+    
+// your code here!
+    
+console.log("there are " + countPositives + " positive values");
+
+
+
+//is the array a Palindrome??
+
+
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+    
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
+    
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
