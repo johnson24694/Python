db71524921d48746d8889c4d9f952e5e93071840
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-02-17 11:20:32 -0600

    New algos

--- Algo_book.js
+++ Algo_book.js
@@ -35,18 +35,50 @@
 
 // Given an array and a value Y count and print the number of array values greater than Y.:
 
-var y = 15;
-var arr = [2, 52, 9, 33, 16, 3, -55];
-var count = 0;
-for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > y) {
-        count++;
+// var y = 15;
+// var arr = [2, 52, 9, 33, 16, 3, -55];
+// var count = 0;
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] > y) {
+//         count++;
         
-    }
+//     }
     
+// }
+// console.log(count);
+
+
+// Given an array, print the max, min and average values for that array.
+
+// var max = 0;
+// var min = 0;
+// var sum = 0;
+// var arr1 = [12, 5, 89, 45, 6, 0, 33];
+// for (var i =0; i < arr1.length; i++) {
+//     if (arr1[i] > max) {
+//         max = arr1[i];
+//     }
+//     if (arr1[i] < min ) {
+//         min = arr1[i];
+//     }
+//     sum = sum + arr1[i];
+//     avg = sum/2;
+// }
+// console.log(max, min, avg);
+
+// Swap String For Array Negative Values
+// Replace any negative array values with 'Dojo'.
+
+
+
+var arr1 = [0, 23, -5, 87, -63, 15];
+
+for (var i = 0; i < arr1.length; i++) {
+    
+    if (arr1[i] < 0) {
+        arr1[i] = "Dojo";
+        }
+       
 }
-console.log(count);
 
-
-
-
+console.log(arr1);
